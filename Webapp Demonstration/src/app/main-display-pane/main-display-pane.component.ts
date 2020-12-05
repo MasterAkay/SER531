c0f217030d93ff72ed8781b648ee92ac43c0ad99
siddharthuppal <suppal2@asu.edu>
2020-12-04 22:47:32 -0700

    Template no updated

--- Webapp Demonstration/src/app/main-display-pane/main-display-pane.component.ts
+++ Webapp Demonstration/src/app/main-display-pane/main-display-pane.component.ts
@@ -41,45 +41,45 @@ export class MainDisplayPaneComponent implements OnInit {
   displayTypeOfQuery(templateId): void{
     if (templateId === 0) {
       this.queryResultToDisplay = 'Ask (ent-pred-obj)';
-    } else if (templateId === 1) {
+    } else if (templateId === 5) {
       this.queryResultToDisplay = 'Ask (ent-pred-obj1 . ent-pred-obj2)';
-    }else if (templateId === 2) {
+    }else if (templateId === 7) {
       this.queryResultToDisplay = 'Count Obj (ent-pred-obj)';
-    }else if (templateId === 3){
-      this.queryResultToDisplay = 'Count ent (ent-pred-obj)';
-    }else if (templateId === 4){
-      this.queryResultToDisplay = '?E is_a Type, ?E pred Obj  value. MAX/MIN (value)';
-    }else if (templateId === 5){
-      this.queryResultToDisplay = '?E is_a Type. ?E pred Obj. ?E-secondClause value. MAX (value)';
-    }else if (templateId === 6){
-      this.queryResultToDisplay = '?E is_a Type. ?E pred Obj. ?E-secondClause value. MIN (value)';
-    }else if (templateId === 7){
-      this.queryResultToDisplay = '(E pred F) prop ?value';
     }else if (templateId === 8){
-      this.queryResultToDisplay = '(E pred ?Obj ) prop value';
-    }else if (templateId === 9){
-      this.queryResultToDisplay = 'select where (ent-pred-obj1 . ent-pred-obj2)';
+      this.queryResultToDisplay = 'Count ent (ent-pred-obj)';
     }else if (templateId === 10){
-      this.queryResultToDisplay = '[]';
+      this.queryResultToDisplay = '?E is_a Type, ?E pred Obj  value. MAX/MIN (value)';
     }else if (templateId === 11){
-      this.queryResultToDisplay = 'E REF ?F . ?F RFG G';
+      this.queryResultToDisplay = '?E is_a Type. ?E pred Obj. ?E-secondClause value. MAX (value)';
+    }else if (templateId === 9){
+      this.queryResultToDisplay = '?E is_a Type. ?E pred Obj. ?E-secondClause value. MIN (value)';
     }else if (templateId === 12){
-      this.queryResultToDisplay = 'C RCD xD . xD RDE ?E';
+      this.queryResultToDisplay = '(E pred F) prop ?value';
     }else if (templateId === 13){
-      this.queryResultToDisplay = 'E REF xF . xF RFG ?G';
-    }else if (templateId === 14){
-      this.queryResultToDisplay = 'ASK ?sbj ?pred ?obj filter ?obj = num';
+      this.queryResultToDisplay = '(E pred ?Obj ) prop value';
+    }else if (templateId === 25){
+      this.queryResultToDisplay = 'select where (ent-pred-obj1 . ent-pred-obj2)';
     }else if (templateId === 15){
-      this.queryResultToDisplay = '<?S P O ; ?S InstanceOf Type>';
+      this.queryResultToDisplay = '[]';
     }else if (templateId === 16){
-      this.queryResultToDisplay = '<S P ?O ; ?O instanceOf Type>';
+      this.queryResultToDisplay = 'E REF ?F . ?F RFG G';
     }else if (templateId === 17){
-      this.queryResultToDisplay = '<?S P O ; ?S instanceOf Type ; contains word >';
+      this.queryResultToDisplay = 'C RCD xD . xD RDE ?E';
     }else if (templateId === 18){
-      this.queryResultToDisplay = '<?S P O ; ?S instanceOf Type ; starts with character >';
+      this.queryResultToDisplay = 'E REF xF . xF RFG ?G';
     }else if (templateId === 19){
+      this.queryResultToDisplay = 'ASK ?sbj ?pred ?obj filter ?obj = num';
+    }else if (templateId === 20){
+      this.queryResultToDisplay = '<?S P O ; ?S InstanceOf Type>';
+    }else if (templateId === 21){
+      this.queryResultToDisplay = '<S P ?O ; ?O instanceOf Type>';
+    }else if (templateId === 22){
+      this.queryResultToDisplay = '<?S P O ; ?S instanceOf Type ; contains word >';
+    }else if (templateId === 23){
+      this.queryResultToDisplay = '<?S P O ; ?S instanceOf Type ; starts with character >';
+    }else if (templateId === 24){
       this.queryResultToDisplay = 'E REF ?F';
-    }else if (templateId === 20) {
+    }else if (templateId === 14) {
       this.queryResultToDisplay = '?D RDE E';
     }
     }
